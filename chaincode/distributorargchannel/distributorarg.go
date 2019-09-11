024bcd342adbceded8b5adf7300d3c69219da09b
Khanh Tran <khanhtrant12@outlook.com>
2019-09-11 12:02:22 +0700

    distributor chaincode update

--- chaincode/distributorargchannel/distributorarg.go
+++ chaincode/distributorargchannel/distributorarg.go
@@ -5,37 +5,27 @@ import (
 	"encoding/json"
 	"fmt"
 	"math"
+	"time"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	sc "github.com/hyperledger/fabric/protos/peer"
 )
 
-// Define the Smart Contract structure
-type DistributorArg struct {
+type distributorArg struct {
 }
 
-// Define the car structure, with 4 properties.  Structure tags are used by encoding/json library
-type Balance struct {
-	UniqueNumber string  `json:"id"`
-	Balance      float64 `json:"balance"`
-	UpdatedDate  string  `json:"updated_date"`
+type balance struct {
+	UniqueNumber string    `json:"id"`
+	Balance      float64   `json:"balance"`
+	UpdatedDate  time.Time `json:"updated_date"`
 }
 
-type Order struct {
-	Cost     float64 `json:"cost"`
+type order struct {
 	Discount float64 `json:"discount"`
 	Total    float64 `json:"total_cost"`
 }
 
-type OrderDetails struct {
-	DrugID   string  `json:"drug_id"`
-	Quantity float64 `json:"quantity"`
-	Cost     float64 `json:"cost"`
-}
-
-const discount = 0.2
-
-func Float64frombytes(bytes []byte) float64 {
+func float64frombytes(bytes []byte) float64 {
 	bits := binary.LittleEndian.Uint64(bytes)
 	float := math.Float64frombits(bits)
 	return float
@@ -45,7 +35,7 @@ func Float64frombytes(bytes []byte) float64 {
  * The Init method is called when the Smart Contract "fabcar" is instantiated by the blockchain network
  * Best practice is to have any Ledger initialization in separate function -- see initLedger()
  */
-func (s *DistributorArg) Init(stub shim.ChaincodeStubInterface) sc.Response {
+func (s *distributorArg) Init(stub shim.ChaincodeStubInterface) sc.Response {
 	return shim.Success(nil)
 }
 
@@ -53,7 +43,7 @@ func (s *DistributorArg) Init(stub shim.ChaincodeStubInterface) sc.Response {
  * The Invoke method is called as a result of an application request to run the Smart Contract "fabcar"
  * The calling application program has also specified the particular smart contract function to be called, with arguments
  */
-func (s *DistributorArg) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
+func (s *distributorArg) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
 
 	// Retrieve the requested Smart Contract function and arguments
 	function, args := APIstub.GetFunctionAndParameters()
@@ -62,18 +52,20 @@ func (s *DistributorArg) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response
 		return s.checkBalance(APIstub, args[0])
 	} else if function == "initBalance" {
 		return s.initBalance(APIstub)
+	} else if function == "payFirst" {
+		return s.payFirst(APIstub, args)
 	}
 	return shim.Error("Invalid Smart Contract function name.")
 }
 
-func (s *DistributorArg) checkBalance(APIstub shim.ChaincodeStubInterface, balanceOwner string) sc.Response {
+func (s *distributorArg) checkBalance(APIstub shim.ChaincodeStubInterface, balanceOwner string) sc.Response {
 	balanceAsBytes, _ := APIstub.GetState(balanceOwner)
 	return shim.Success(balanceAsBytes)
 }
 
-func (s *DistributorArg) initBalance(APIstub shim.ChaincodeStubInterface) sc.Response {
-	feedexBalance := Balance{UniqueNumber: "9007733586", Balance: 9999999}
-	viepharmaBalance := Balance{UniqueNumber: "5819654223", Balance: 9999999}
+func (s *distributorArg) initBalance(APIstub shim.ChaincodeStubInterface) sc.Response {
+	feedexBalance := balance{UniqueNumber: "9007733586", Balance: 9999999}
+	viepharmaBalance := balance{UniqueNumber: "5819654223", Balance: 9999999}
 	feedexBalanceAsBytes, _ := json.Marshal(feedexBalance)
 	viepharmaBalanceAsByte, _ := json.Marshal(viepharmaBalance)
 	APIstub.PutState("feedexCorpBalance", feedexBalanceAsBytes)
@@ -82,55 +74,55 @@ func (s *DistributorArg) initBalance(APIstub shim.ChaincodeStubInterface) sc.Res
 	return shim.Success(nil)
 }
 
-// func (s *DistributorArg) buyDrug(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
-// 	//pay 30% money then wait the confirm
-// 	//pay 30%
-// 	//input: list of order detail
-// 	//Get orderdetails
-// 	var orderdetails = GetOrderDetails(args)
-// 	//Calculate pay total
-// 	//get current balance
-// 	balanceAsBytes, _ := APIstub.GetState("feedexCorpBalance")
-// 	currentBalance := float64(balanceAsBytes.balance)
-// 	//get total pay
-// 	var total float64
-// 	total = 0
-// 	for i := 0; i < len(orderdetails); i++ {
-// 		total += orderdetails[i].Cost * orderdetails[i].Quantity
-// 	}
-// 	//new balance with 20%
-// 	newBalance := currentBalance - total*0.3
+//input: args[0] - ownerId, args[1] - order, args[2]
+//output: new balance
+func (s *distributorArg) payFirst(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+	data := order{}
 
-// 	return shim.Success(nil)
+	err := json.Unmarshal([]byte(args[2]), &data)
 
-// }
+	if err != nil {
+		fmt.Println(err)
+	}
 
-// func (s *DistributorArg) confirmBuyDrug(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
-// 	//if all the orders has deliveried, confirm the order
-// 	//pay 70% money then change the owner
-// 	//pay 70%
-// 	//input current balance, 70% money of total
+	s.updateBalance(APIstub, args[0], data.Discount, data.Total)
 
-// 	//output new balance
-// }
+	return shim.Success(nil)
+}
 
-// func GetOrderDetails(args []string) []OrderDetails {
-// 	var data = make([]OrderDetails, len(args))
+func (s *distributorArg) getCurrentBalance(APIstub shim.ChaincodeStubInterface, ownerID string) float64 {
+	balanceAsBytes, _ := APIstub.GetState(ownerID)
+	currentbalance := balance{}
 
-// 	for i := 0; i < len(args); i++ {
-// 		err := json.Unmarshal([]byte(args[i]), &data[i])
-// 		if err != nil {
-// 			fmt.Print(err)
-// 		}
-// 	}
-// 	return data
-// }
+	json.Unmarshal(balanceAsBytes, &currentbalance)
+	return currentbalance.Balance
+}
+
+func (s *distributorArg) updateBalance(APIstub shim.ChaincodeStubInterface, ownerID string, discount float64, minus float64) balance {
+	//args[0]: key
+	//args[1]: minus balance
+	balanceAsBytes, err := APIstub.GetState(ownerID)
+	currentbalance := balance{}
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	json.Unmarshal(balanceAsBytes, &currentbalance)
+	newBalance := currentbalance.Balance - minus*discount
+
+	currentbalance.Balance = newBalance
+	currentbalance.UpdatedDate = time.Now()
+
+	APIstub.PutState(ownerID, balanceAsBytes)
+
+	return currentbalance
+}
 
 // The main function is only relevant in unit test mode. Only included here for completeness.
 func main() {
 
 	// Create a new Smart Contract
-	err := shim.Start(new(DistributorArg))
+	err := shim.Start(new(distributorArg))
 	if err != nil {
 		fmt.Printf("Error creating new Smart Contract: %s", err)
 	}
--- testing.sh
+++ testing.sh
@@ -50,8 +50,32 @@ docker exec \
   -e CORE_PEER_MSPCONFIGPATH=${VIEPHARMACORP_MSPCONFIGPATH} \
   -e CORE_PEER_TLS_ROOTCERT_FILE=${VIEPHARMACORP_TLS_ROOTCERT_FILE} \
   cli \
-    peer chaincode invoke -o orderer.drugchain.com:7050 -C distributorargchannel -n distributorcc -c '{"function":"seeBalance","Args":["feedexCorpbalance"]}' \
+    peer chaincode invoke -o orderer.drugchain.com:7050 -C distributorargchannel -n distributorcc -c '{"function":"checkBalance","Args":["feedexCorpbalance"]}' \
     --tls \
     --cafile ${ORDERER_TLS_ROOTCERT_FILE} \
     --peerAddresses peer0.viepharmacorp.com:7051 \
     --tlsRootCertFiles ${VIEPHARMACORP_TLS_ROOTCERT_FILE}
+
+docker exec \
+  -e CORE_PEER_LOCALMSPID=ViePharmaCorpMSP \
+  -e CORE_PEER_ADDRESS=peer0.viepharmacorp.com:7051 \
+  -e CORE_PEER_MSPCONFIGPATH=${VIEPHARMACORP_MSPCONFIGPATH} \
+  -e CORE_PEER_TLS_ROOTCERT_FILE=${VIEPHARMACORP_TLS_ROOTCERT_FILE} \
+  cli \
+    peer chaincode invoke -o orderer.drugchain.com:7050 -C distributorargchannel -n distributorcc -c '{"function":"checkBalance","Args":["feedexCorpbalance"]}' \
+    --tls \
+    --cafile ${ORDERER_TLS_ROOTCERT_FILE} \
+    --peerAddresses peer0.viepharmacorp.com:7051 \
+    --tlsRootCertFiles ${VIEPHARMACORP_TLS_ROOTCERT_FILE}
+
+docker exec \
+  -e CORE_PEER_LOCALMSPID=FeedexCorpMSP \
+  -e CORE_PEER_ADDRESS=peer0.feedexcorp.com:9051 \
+  -e CORE_PEER_MSPCONFIGPATH=${FEEDEXCORP_MSPCONFIGPATH} \
+  -e CORE_PEER_TLS_ROOTCERT_FILE=${FEEDEXCORP_TLS_ROOTCERT_FILE} \
+  cli \
+    peer chaincode invoke -o orderer.drugchain.com:7050 -C distributorargchannel -n distributorcc -c `{"function":"payFirst","Args":["feedexCorpbalance",`{"discount":0.2,"total":100}`]}` \
+    --tls \
+    --cafile ${ORDERER_TLS_ROOTCERT_FILE} \
+    --peerAddresses peer0.feedexcorp.com:9051 \
+    --tlsRootCertFiles ${FEEDEXCORP_TLS_ROOTCERT_FILE}
